db9e886b9e29977c2358414fd337c4931d698712
Hdhdjd Djdjsdjdj <hdhdjddjdjsdjdj@Hdhdjds-MacBook-Pro.local>
2023-07-23 16:23:00 -0400

    first-commit

---
+++ src/app/asset/icons.tsx
@@ -0,0 +1,9 @@
+import { BiLogoTwitter, BiHomeCircle, BiSearch, BiBell, BiEnvelope } from 'react-icons/bi';
+import { RiFileListLine } from 'react-icons/ri';
+import { PiBookmarkSimpleBold } from 'react-icons/pi';
+import { BsPeople, BsPerson } from 'react-icons/bs';
+import { LuVerified } from 'react-icons/lu';
+import { CgMoreO } from 'react-icons/cg';
+import { IoIosMore } from "react-icons/io";
+
+export { BiLogoTwitter, BiHomeCircle, BiSearch, BiBell, BiEnvelope, RiFileListLine, PiBookmarkSimpleBold, BsPeople, BsPerson, LuVerified, CgMoreO, IoIosMore };
